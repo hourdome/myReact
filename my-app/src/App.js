9a366adb9d6ae9ebc39c0050b8689888da19b30e
hourdome <saengmueng_90@hotmail.com>
2023-05-24 10:29:20 +0700

    Project 2.2 UI Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,17 +3,13 @@
 import logo from './logo.svg'
 import Header from './Components/Topbar'
 import ControlCard from './Components/ControlCard'
-import { Button, Card, CardContent } from '@mui/joy'
 
 export default function App() {
   const titleArray = ['Logistic', 'E-commerce', 'Banking']
 
   return (
     <div>
-      <Header
-        className='text-3xl font-bold underline'
-        subtitle='สวัสดีเราคือ Subtitle'
-      />
+      <Header className='text-3xl font-bold underline' subtitle=' TEST' />
       {titleArray.map((eachTitleArray) => (
         <ControlCard title={eachTitleArray} />
       ))}
--- my-app/src/Components/ControlCard.js
+++ my-app/src/Components/ControlCard.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import { Button, Card, CardContent } from '@mui/joy'
+import { Alert, ToggleSwitch } from 'flowbite-react'
 
 export default function ControlCard({ title }) {
-  const alertStart = () => {
-    window.alert('You are pressing ' + title)
+  const startProgram = () => {
+    return (
+      <Alert
+        color='success'
+        onDismiss={function onDismiss() {
+          return alert('Alert dismissed')
+        }}
+      >
+        <span>
+          <span className='font-medium'>Info alert</span> Change a few things up
+          and try submitting again.
+        </span>
+      </Alert>
+    )
+  }
+
+  const restartProgram = () => {
+    window.alert('Restart ' + title)
   }
   const showData = () => {
     return (
@@ -15,20 +32,21 @@ export default function ControlCard({ title }) {
   }
   return (
     <Card className='m-4 rounded-md border'>
-      <h4>{title}</h4>
-      <button
-        className='text-base p-2 font-bold bg-green-500 rounded-sm'
-        onClick={() => alertStart()}
-      >
-        Start
-      </button>
-      <button className='text-base p-2 font-bold bg-yellow-500 rounded-sm'>
-        Restart
-      </button>
-      <button className='text-base p-2 font-bold bg-orange-500 rounded-sm'>
-        Quit
-      </button>
-      {showData()}
+      <CardContent>
+        <h4>{title}</h4>
+        <div className='flex flex-wrap gap-2' id='toggle'>
+          <Button pill={true} onClick={() => startProgram()}>
+            Start
+          </Button>
+          <Button pill={true} color='warning' onClick={() => restartProgram()}>
+            Restart
+          </Button>
+          <Button pill={true} color='danger'>
+            Down
+          </Button>
+        </div>
+        {showData()}
+      </CardContent>
     </Card>
   )
 }
